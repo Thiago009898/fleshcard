9faa3c23e2a19157ca00c2c2648291eafce78403
Thiago009898 <thiago.kosiol@escola.pr.gov.br>
2025-08-06 16:13:43 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'quanto é 10x10?',
+    'A resposta é 100'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Parana?'
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ovo em inglês?',
+    'ovo em ingles é egg (RAI)'
+)
